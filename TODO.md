5ec6bc7ad3f713e9170745d10c5eb8cf24bcc620
Tobias Gottschalk <gottschalk.tobias98@outlook.de>
2024-06-13 12:44:37 +0200

    added todo

--- TODO.md
+++ TODO.md
@@ -1,2 +1,4 @@
 - Change "src/home.svg" possible into another svg.
 - Change homepage svg's.
+- Add searchbar based on meta/yaml - possible "fuzzy search".
+
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://gottschalktobias1998.github.io/RedPandaDocs.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/RedPandaDocs.io',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'gottschalktobias1998', // Usually your GitHub org/user name.
+  projectName: 'RedPandaDocs.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/gottschalktobias1998/RedPandaDocs.io',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/gottschalktobias1998/RedPandaDocs.io/tree/main',
         },
         theme: {
           customCss: './src/css/custom.css',
